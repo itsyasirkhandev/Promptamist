62aedec92cd39c92e965dc51fa10a01fafd52476
Yasir Khan <yasirwebio@gmail.com>
2026-01-24 13:10:57 +0000

    updated the fav icon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -55,10 +55,6 @@ export default function RootLayout({
     <html lang="en" suppressHydrationWarning>
       <head>
         <meta name="google-site-verification" content="c1VKq-JyWZuq4XwG28KMXYU0GrmNJC628POSo4BnZh8" />
-        <link rel="icon" href="/favicon.ico" sizes="any" />
-        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
         <link rel="manifest" href="/manifest.json" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
